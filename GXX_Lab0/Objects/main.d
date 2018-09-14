d8eb12d3fdfc19c6c1984275f5400b84009289b5
thomasp05 <thomas.philippon@mail.mcgill.ca>
2018-09-14 12:09:06 -0400

    try 1

---
+++ GXX_Lab0/Objects/main.d
@@ -0,0 +1,2 @@
+.\objects\main.o: src\main.c
+.\objects\main.o: C:\School Programs\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
---
+++ GXX_Lab0/src/moving_avg.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int moving_avg(float *sig, float *sigf, int N, int D){
+	
+			int i, j;
+			int	a = D/2;
+	
+	for(i = 0; i<N; i++){
+		
+		
+		float tempsig = *(sig + i);
+		
+
+			if(D % 2 == 0){  //even
+				j= i-a;
+			}
+			else {
+					
+		}
+		for(j = 0;j < D; j++){
+			
+			
+			
+		}
+				
+	
+	}
+	
+	
+	
+	
+}
