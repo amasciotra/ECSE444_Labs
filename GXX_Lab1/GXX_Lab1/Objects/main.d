88b7fee4319e0c2ba767a9b7dc6885059ba01c43
amasciotra <alexmasciotra@gmail.com>
2018-09-17 14:19:42 -0400

    dotproduct

--- GXX_Lab1/GXX_Lab1/Objects/main.d
+++ GXX_Lab1/GXX_Lab1/Objects/main.d
@@ -8,4 +8,3 @@
 .\objects\main.o: C:\Keil_v5\ARM\PACK\ARM\CMSIS\5.4.0\CMSIS\Core\Include\cmsis_armcc.h
 .\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\string.h
 .\objects\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\math.h
-.\objects\main.o: ./inc/asm_max.h
